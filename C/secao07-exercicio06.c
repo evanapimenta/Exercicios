be74dd79275f698b8dbec190697ecdf032866d5a
Evana Pimenta Michilotti <evana.pimenta@hotmail.com>
2023-06-23 09:04:03 -0300

    .

---
+++ C/secao07-exercicio06.c
@@ -0,0 +1,10 @@
+// Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual número ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo.
+// Tabuada do 5:
+// 5 x 1 = 5
+// 5 x 2 = 10
+// ...
+// 5 x 10 = 50
+
+#include <stdio.h>
+
+int 
